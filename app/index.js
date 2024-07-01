5dd9f7e11227c23e6ba6b8ebf329d98d897c05ca
Jeremy Coplen <jeremycoplen.jc@gmail.com>
2024-07-01 03:15:24 -0600

    week day containers setup

--- app/index.js
+++ app/index.js
@@ -5,7 +5,36 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <View style={styles.week}>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Monday</Text>
+          <View style={styles.showsContainer}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Tuesday</Text>
+          <View style={styles.showsContainer}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Wednesday</Text>
+          <View style={styles.showsContainer}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Thursday</Text>
+          <View style={styles.showsContainer}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Friday</Text>
+          <View style={styles.showsContainer}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Saturday</Text>
+          <View style={styles.showsContainer}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Sunday</Text>
+          <View style={styles.showsContainer}></View>
+        </View>
+      </View>
       <Link style={styles.text} href="/add">Add</Link>
       <StatusBar style="light" />
     </View>
@@ -18,9 +47,35 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    width: "100%"
   },
-
   text: {
     color: "#fff"
+  },
+  week: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "100%",
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#1A1A1A",
+    width: "80%",
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 5
+  },
+  showsContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  dayText: {
+    color: "#fff",
+    fontWeight: "700",
+    fontSize: 20
   }
 });
